f33209f3dc3f94696ad5b8190eb76416af67a895
EaswarGn <easwar.chess@gmail.com>
2024-08-25 13:57:05 -0700

    fixed issue #12

--- chess_transformers/train/datasets.py
+++ chess_transformers/train/datasets.py
@@ -104,80 +104,70 @@ class ChessDataset(Dataset):
 
 class ChessDatasetFT(Dataset):
     def __init__(self, data_folder, h5_file, split, **unused):
-        """
-        Init.
-
-        Args:
-
-            data_folder (str): The folder containing the H5 and splits
-            files.
-
-            h5_file (str): The H5 file.
-
-            split (str): The data split. One of "train", "val", None.
-            Defaults to None, which means that all datapoints will be
-            included.
-        """
-        # Open table in H5 file
-        self.h5_file = tb.open_file(os.path.join(data_folder, h5_file), mode="r")
-        self.encoded_table = self.h5_file.root.encoded_data
+        self.data_folder = data_folder
+        self.h5_file_path = os.path.join(data_folder, h5_file)
         self.split = split
-
-        # Create indices
         if split == "train":
             self.first_index = 0
         elif split == "val":
-            self.first_index = self.encoded_table.attrs.val_split_index
+            self.first_index = None  # Set later
         elif split is None:
             self.first_index = 0
         else:
             raise NotImplementedError
 
     def __getitem__(self, i):
-        turns = torch.IntTensor([self.encoded_table[self.first_index + i]["turn"]])
-        white_kingside_castling_rights = torch.IntTensor(
-            [self.encoded_table[self.first_index + i]["white_kingside_castling_rights"]]
-        )  # (1)
-        white_queenside_castling_rights = torch.IntTensor(
-            [self.encoded_table[self.first_index + i]["white_queenside_castling_rights"]]
-        )  # (1)
-        black_kingside_castling_rights = torch.IntTensor(
-            [self.encoded_table[self.first_index + i]["black_kingside_castling_rights"]]
-        )  # (1)
-        black_queenside_castling_rights = torch.IntTensor(
-            [self.encoded_table[self.first_index + i]["black_queenside_castling_rights"]]
-        )  # (1)
-        board_position = torch.IntTensor(
-            self.encoded_table[self.first_index + i]["board_position"]
-        )  # (64)
-        from_square = torch.LongTensor(
-            [self.encoded_table[self.first_index + i]["from_square"]]
-        )  # (1)
-        to_square = torch.LongTensor(
-            [self.encoded_table[self.first_index + i]["to_square"]]
-        )  # (1)
-        length = torch.LongTensor([1])
-        return {
-            "turns": turns,
-            "white_kingside_castling_rights": white_kingside_castling_rights,
-            "white_queenside_castling_rights": white_queenside_castling_rights,
-            "black_kingside_castling_rights": black_kingside_castling_rights,
-            "black_queenside_castling_rights": black_queenside_castling_rights,
-            "board_positions": board_position,
-            "from_squares": from_square,
-            "to_squares": to_square,
-            "lengths": length,
-        }
+        with tb.open_file(self.h5_file_path, mode="r") as h5_file:
+            encoded_table = h5_file.root.encoded_data
+            first_index = self.first_index
+            if self.split == "val":
+                first_index = encoded_table.attrs.val_split_index
+            turns = torch.IntTensor([encoded_table[first_index + i]["turn"]])
+            white_kingside_castling_rights = torch.IntTensor(
+                [encoded_table[first_index + i]["white_kingside_castling_rights"]]
+            )  # (1)
+            white_queenside_castling_rights = torch.IntTensor(
+                [encoded_table[first_index + i]["white_queenside_castling_rights"]]
+            )  # (1)
+            black_kingside_castling_rights = torch.IntTensor(
+                [encoded_table[first_index + i]["black_kingside_castling_rights"]]
+            )  # (1)
+            black_queenside_castling_rights = torch.IntTensor(
+                [encoded_table[first_index + i]["black_queenside_castling_rights"]]
+            )  # (1)
+            board_position = torch.IntTensor(
+                encoded_table[first_index + i]["board_position"]
+            )  # (64)
+            from_square = torch.LongTensor(
+                [encoded_table[first_index + i]["from_square"]]
+            )  # (1)
+            to_square = torch.LongTensor(
+                [encoded_table[first_index + i]["to_square"]]
+            )  # (1)
+            length = torch.LongTensor([1])
+            return {
+                "turns": turns,
+                "white_kingside_castling_rights": white_kingside_castling_rights,
+                "white_queenside_castling_rights": white_queenside_castling_rights,
+                "black_kingside_castling_rights": black_kingside_castling_rights,
+                "black_queenside_castling_rights": black_queenside_castling_rights,
+                "board_positions": board_position,
+                "from_squares": from_square,
+                "to_squares": to_square,
+                "lengths": length,
+            }
 
     def __len__(self):
-        if self.split == "train":
-            return self.encoded_table.attrs.val_split_index
-        elif self.split == "val":
-            return self.encoded_table.nrows - self.encoded_table.attrs.val_split_index
-        elif self.split is None:
-            self.encoded_table.nrows
-        else:
-            raise NotImplementedError
+        with tb.open_file(self.h5_file_path, mode="r") as h5_file:
+            encoded_table = h5_file.root.encoded_data
+            if self.split == "train":
+                return encoded_table.attrs.val_split_index
+            elif self.split == "val":
+                return encoded_table.nrows - encoded_table.attrs.val_split_index
+            elif self.split is None:
+                return encoded_table.nrows
+            else:
+                raise NotImplementedError
 
 
 
