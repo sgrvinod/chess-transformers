c88dba95f83027d646733d4ffd50e8fa60adddae
Sagar Vinodababu <sgrvinod@gmail.com>
2025-01-16 18:30:24 -0700

    add average_checkpoints() to __init__.py for easier access

--- chess_transformers/train/__init__.py
+++ chess_transformers/train/__init__.py
@@ -1,3 +1,4 @@
 __all__ = ["utils", "datasets", "average_checkpoints", "train"]
 
 from chess_transformers.train.train import train_model
+from chess_transformers.train.average_checkpoints import average_checkpoints
